faf610ace1001fcf4d03dca7bcd757a5f89957cd
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2026-04-22 17:31:36 +0500

    feat(logging): integrate winston and loki for enhanced logging
    - Added nest-winston and winston-loki dependencies to package.json.
    - Configured Winston logger in a new winston.config.ts file.
    - Updated main.ts to use the custom Winston logger.
    - Enhanced PrismaService to log database queries and events using Winston.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
-import "reflect-metadata";
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
-import { ValidationPipe } from "@nestjs/common";
 import { join } from "path";
 import { AppModule } from "./app.module";
+import { createWinstonLogger } from "./common/logger/winston.config";
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: createWinstonLogger(),
+  });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: false, transform: true }));
 
@@ -19,7 +21,9 @@ async function bootstrap() {
   });
 
   const port = process.env.PORT || 3000;
+
   await app.listen(port);
+
   console.log(`Server running on http://localhost:${port}`);
 }
 
